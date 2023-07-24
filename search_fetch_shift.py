dc6fcd1e9d160c61dc9b04b9d5475b5c2b164e95
AS-Coope <deltazirconium@gmail.com>
2023-07-23 19:24:21 -0500

    Separating directory existence check in separate functions for separation of concerns

--- search_fetch_shift.py
+++ search_fetch_shift.py
@@ -5,12 +5,26 @@ from os.path import isfile, join
 
 # your desired source and destination directories are to be placed here respectively
 # Example of file path: "C:\\Users\\your_user_name\\Downloads" [make sure that there are 2 backslashes]
-src_dir = ""
+src_dir = "C:\\Users\\Test_Downloads_Folders"
 dest_dir = ""
+
+def create_dest_dir():
+    # creating the destination directory if it does not exist yet (as a failsafe)
+    if not path.exists(dest_dir):
+        os.makedirs(dest_dir)
+        
+def create_src_dir():
+    # creating the source directory if it does not exist yet (as a failsafe)
+    if not path.exists(src_dir):
+        os.makedirs(src_dir)
 
 # lists to hold files and folders in src directory
 all_items_in_src = listdir(src_dir)
 all_files_in_src = []
+
+# creating the source and destination directories if it does not exist yet (as a failsafe)
+create_src_dir()
+create_dest_dir()
 
 def get_all_files():
     for item in all_items_in_src:
@@ -20,10 +34,6 @@ def get_all_files():
             all_files_in_src.append(item)
 
 def shift_image_files():
-
-    # creating the destination directory if it does not exist yet (as a failsafe)
-    if not path.exists(dest_dir):
-        os.makedirs(dest_dir)
     
     get_all_files()
 
@@ -35,9 +45,6 @@ def shift_image_files():
             print(file, "successfully moved from", src_dir, "to", dest_dir, "!")
 
 def shift_pdf_files():
-    # creating the destination directory if it does not exist yet (as a failsafe)
-    if not path.exists(dest_dir):
-        os.makedirs(dest_dir)
 
     # find all image files (specifically .jpg and .png)
     for file in all_files_in_src:
