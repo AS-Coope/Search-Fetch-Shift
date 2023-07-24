f00a4efa5ca3a036e3fc8945f956329e7bcf44b2
AS-Coope <deltazirconium@gmail.com>
2023-07-23 19:44:05 -0500

    Switch source and destination directory paths

--- search_fetch_shift.py
+++ search_fetch_shift.py
@@ -3,13 +3,17 @@ import shutil
 from os import path, listdir
 from os.path import isfile, join
 
+######## Directory path setup ########
 # your desired source and destination directories are to be placed here respectively
 # Example of file path: "C:\\Users\\your_user_name\\Downloads" [make sure that there are 2 backslashes]
-src_dir = "C:\\Users\\Test_Downloads_Folders"
-dest_dir = ""
-pdf_dest_dir = "C:\\Users\\Test_Downloads_PDFs"
-imgs_dest_dir = "C:\\Users\\Test_Downloads_Images"
 
+## NOTE: The PUBLIC folder in the C drive is available to all users of the system ##
+src_dir = "C:\\Users\\Public\\Public Downloads"
+dest_dir = "C:\\Users\\Public\\Public Documents"
+pdf_dest_dir = "C:\\Users\\Public\\Public Documents"
+imgs_dest_dir = "C:\\Users\\Public\\Public Documents"
+
+######## Directory Existence Checks ########
 def create_dest_dir(the_dest_dir):
     # creating the destination directory if it does not exist yet (as a failsafe)
     if not path.exists(the_dest_dir):
@@ -20,13 +24,15 @@ def create_src_dir():
     if not path.exists(src_dir):
         os.makedirs(src_dir)
 
+# creating the source directory if it does not exist yet (as a failsafe)
+create_src_dir()
+
+######## List Declarations for file storage ########
 # lists to hold files and folders in src directory
 all_items_in_src = listdir(src_dir)
 all_files_in_src = []
 
-# creating the source directory if it does not exist yet (as a failsafe)
-create_src_dir()
-
+######## Business Logic for file shifting ########
 def get_all_files():
     for item in all_items_in_src:
 
@@ -49,7 +55,7 @@ def shift_pdf_files(pdf_func_dest_dir):
 
     get_all_files()
     create_dest_dir(pdf_func_dest_dir)
-    
+
     # find all image files (specifically .jpg and .png)
     for file in all_files_in_src:
         # searching specifically for image files and MOVING them from the source directory to the destination directory 
@@ -57,5 +63,6 @@ def shift_pdf_files(pdf_func_dest_dir):
             shutil.move(join(src_dir, file), pdf_func_dest_dir)
             print(file, "successfully moved from", src_dir, "to", pdf_func_dest_dir, "!")
 
-shift_image_files()
-shift_pdf_files()
+######## Function Calls ########
+shift_image_files(dest_dir)
+shift_pdf_files(dest_dir)
