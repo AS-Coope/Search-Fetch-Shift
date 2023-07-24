26edb3599d28956ecd49f1a85caa69c6b5459902
AS-Coope <deltazirconium@gmail.com>
2023-07-23 19:33:56 -0500

    Change implementation of shift functions to accept a directory as a parameter

--- search_fetch_shift.py
+++ search_fetch_shift.py
@@ -12,8 +12,8 @@ imgs_dest_dir = "C:\\Users\\Test_Downloads_Images"
 
 def create_dest_dir(the_dest_dir):
     # creating the destination directory if it does not exist yet (as a failsafe)
-    if not path.exists(dest_dir):
-        os.makedirs(dest_dir)
+    if not path.exists(the_dest_dir):
+        os.makedirs(the_dest_dir)
         
 def create_src_dir():
     # creating the source directory if it does not exist yet (as a failsafe)
@@ -24,9 +24,8 @@ def create_src_dir():
 all_items_in_src = listdir(src_dir)
 all_files_in_src = []
 
-# creating the source and destination directories if it does not exist yet (as a failsafe)
+# creating the source directory if it does not exist yet (as a failsafe)
 create_src_dir()
-create_dest_dir()
 
 def get_all_files():
     for item in all_items_in_src:
@@ -35,25 +34,28 @@ def get_all_files():
         if(isfile(join(src_dir, item))):
             all_files_in_src.append(item)
 
-def shift_image_files():
+def shift_image_files(img_func_dest_dir):
     
     get_all_files()
-
+    create_dest_dir(img_func_dest_dir)
     # find all image files (specifically .jpg and .png)
     for file in all_files_in_src:
         # searching specifically for image files and MOVING them from the source directory to the destination directory 
         if ( join(src_dir, file).endswith( ('.jpg', '.png') ) ):
-            shutil.move(join(src_dir, file), dest_dir)
-            print(file, "successfully moved from", src_dir, "to", dest_dir, "!")
+            shutil.move(join(src_dir, file), img_func_dest_dir)
+            print(file, "successfully moved from", src_dir, "to", img_func_dest_dir, "!")
 
-def shift_pdf_files():
+def shift_pdf_files(pdf_func_dest_dir):
 
+    get_all_files()
+    create_dest_dir(pdf_func_dest_dir)
+    
     # find all image files (specifically .jpg and .png)
     for file in all_files_in_src:
         # searching specifically for image files and MOVING them from the source directory to the destination directory 
         if ( join(src_dir, file).endswith( '.pdf' ) ):
-            shutil.move(join(src_dir, file), dest_dir)
-            print(file, "successfully moved from", src_dir, "to", dest_dir, "!")
+            shutil.move(join(src_dir, file), pdf_func_dest_dir)
+            print(file, "successfully moved from", src_dir, "to", pdf_func_dest_dir, "!")
 
 shift_image_files()
 shift_pdf_files()
