be74cf0e21a1bfe9d6e544454c8d5914cd27f963
AS-Coope <deltazirconium@gmail.com>
2024-01-23 14:43:57 -0500

    Implement functionality to read a file extension

--- search_fetch_shift.py
+++ search_fetch_shift.py
@@ -11,8 +11,8 @@ from os.path import isfile, join
 ## NOTE: The PUBLIC folder in the C drive is available to all users of the system ##
 public_downloads = "C:\\Users\\Public\\Public Downloads"
 public_documents = "C:\\Users\\Public\\Public Documents" 
-src_dir = public_documents
-dest_dir = public_downloads
+src_dir = public_downloads
+dest_dir = public_documents
 pdf_dest_dir = public_downloads
 imgs_dest_dir = public_downloads
 
@@ -72,6 +72,13 @@ def shift_pdf_files(pdf_func_dest_dir):
             shutil.move(join(src_dir, file), pdf_func_dest_dir)
             print(file, "successfully moved from", src_dir, "to", pdf_func_dest_dir, "!")
 
+def shift_files(file_func_dest_dir, file_ext):
+    for file in all_files_in_src:
+            # searching specifically for files of a specified file type/extension and MOVING them from the source directory to the destination directory 
+            if ( join(src_dir, file).endswith( file_ext ) ):
+                shutil.move(join(src_dir, file), file_func_dest_dir)
+                print(file, "successfully moved from", src_dir, "to", file_func_dest_dir, "!")
+
 def exitProgram():
     print("Program now closing!")
     sys.exit()
@@ -80,24 +87,31 @@ def exitProgram():
 # dictionary that selects a specific 'shift' function based on the user's choice in the main menu
 option = {
     1: shift_image_files,
-    2: shift_pdf_files
+    2: shift_pdf_files,
+    3: shift_files
 }
 
 # dictonary that selects the directory to push files to depending on the user's input
 dest_dirs = {
     1 : imgs_dest_dir,
-    2 : pdf_dest_dir
+    2 : pdf_dest_dir,
+    3 : dest_dir
 }
 
 def main_menu():
     print("|Search Fetch Shift")
     print("|1. Moves Image Files (.png, .jpg)")
     print("|2. Move PDF files (.pdf)")
+    print("|3. Move files of another type")
     print("|0. Exit program")
     optionVal = int(input("|Enter the number beside the operation you want to perform: "))
 
     if (optionVal == 0):
         exitProgram()
+    elif(optionVal == 3):
+        file_ext_input = input("Enter the file extension (Do not add the period in front of the extension)\n" + 
+              "Example: to use text files, enter 'txt' instead of '.txt': ")
+        option[optionVal](dest_dirs[optionVal], file_ext_input)
     else:
         option[optionVal](dest_dirs[optionVal])
 
