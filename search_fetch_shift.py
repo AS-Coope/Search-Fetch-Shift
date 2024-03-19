01db7a5e0cdf31de7f9b9f8b0898190a2bc16c51
AS-Coope <deltazirconium@gmail.com>
2024-03-19 14:05:15 -0500

    Create Exception to handle the sample.json file missing

--- search_fetch_shift.py
+++ search_fetch_shift.py
@@ -119,16 +119,21 @@ def main_menu():
     elif(optionVal == 4):
         dir_name = input("Please enter the directory name: ")
         dir_path = input("Please add the directory's absolute path: ")
-
-        with open('sample.json', 'r') as openfile:
-        # Reading from json file
-            saved_dirs = json.load(openfile)
+        try:
+            with open('sample.json', 'r') as openfile:
+                # Reading from json file
+                saved_dirs = json.load(openfile)
         
-        saved_dirs[dir_name] = dir_path
-        y = json.dumps(saved_dirs)
-        print(y)
-        with open("sample.json", "w") as outfile:
-            outfile.write(y)
+            saved_dirs[dir_name] = dir_path
+            y = json.dumps(saved_dirs)
+            print(y)
+            with open("sample.json", "w") as outfile:
+                outfile.write(y)
+        except Exception as e:
+            print(e)
+            print("An error occurred while trying to open the JSON file.")
+            print("The file may not have been created.")
+        
 
     elif(optionVal == 5):
         with open('sample.json', 'r') as openfile:
